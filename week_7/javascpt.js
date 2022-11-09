713a21580fae930fee2e6db8d97889562ed5ab29
alexander gordon <alexandergordon200@gmail.com>
2022-11-09 00:54:10 +0000

    push push in lap

--- week_7/javascpt.js
+++ week_7/javascpt.js
@@ -12,6 +12,7 @@ title.classList.add("red_background");
 var x = 0   //the value that is taken from the text bar 
  
 
+let billValue = document.getElementById("myInputBox").value;
 
 
  
@@ -43,3 +44,26 @@ function Fivepercent(){
 
     displayEl.innerHTML = "£" + x; 
 }
+
+
+function twentfiveORpercent(){
+
+    var x = document.getElementById("textBox").value; 
+    var y = Math.floor((Math.random() * 2) + 1);
+    
+    
+    if (y == 1){
+
+        x = x * 0.25;
+    }
+    else if (y == 2){
+
+        x = (x * 0.1) + 2 
+    }
+    
+
+    displayEl.innerHTML = "£" + x; 
+}
+
+
+
