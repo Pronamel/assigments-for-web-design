7881dc92e23e92c8a468f5a653d4fbdf79e7e6a8
alexander gordon <alexandergordon200@gmial.com>
2022-11-14 15:00:51 +0000

    week 8 so far

---
+++ week_8/java.js
@@ -0,0 +1,14 @@
+
+
+
+// JQuery
+$("#orangeButton").click(function() {
+    $('#id1').css('color', 'orange');
+  });
+  
+// Pure Javascript
+//  document.getElementById("orangeButton").addEventListener("click", orangeButton);
+
+// function orangeButton() {
+//   document.getElementById('id1').style.color = 'orange';
+// }
